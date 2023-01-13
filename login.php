41b1a948c333d679bed801767524166964521e8e
jacobSalazku <Cooljss10@gmail.com>
2023-01-13 13:30:48 +0100

    init commit login page

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+require('./config/db.php');
+    //als $_post register niet leeg is
+    if(isset($_POST['register'])){
+        //database connect
+
+
+       // $username= $_POST['username'];
+      //  $email= $_POST['email'];
+       // $password= $_POST['password'];
+       // beveiligen tegen xss en sql  
+        $username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
+        $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+        $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
+        $passwordhashed = password_hash($password,PASSWORD_DEFAULT);
+
+
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            //query zoeken of de email al in de database voorkomt
+            $stmt = $conn -> prepare('SELECT * FROM users WHERE email = ?');
+            $stmt -> execute([$email]);
+            $totalUsers = $stmt -> rowCount();
+           // echo $totalUsers. '<br>';
+        }
+        
+        if($totalUsers> 0){
+            
+            $emailTaken= "Email already taken";
+        }
+        else{
+            $stmt = $conn -> prepare('INSERT INTO users(username,email,password) VALUES (?,?,? )');
+            $stmt -> execute([$username,$email,$passwordhashed]);
+        }
+    }   
+
+
+
+?>
+
+<?php require('./includes/header.html'); ?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header bg-light mb-3">Register</div>
+        <div class="card-body">
+            <form action="login.php" method ="POST">
+
+                <div class="from-group">
+                    <label for="email">E-mail</label>
+                    <input required type="email" name="email"  class="form-control">
+                    <br />
+                    <?php if (isset($emailTaken)) :?>
+                        <p style="color:red"><?php echo  $emailTaken; ?></p>
+                        <?php endif; ?>
+                </div>
+
+                <div class="from-group">
+                    <label for="password">Password</label>
+                    <input required type="password" name="password"  class="form-control">
+                </div>
+                <button type="submit" name="login" class="btn btn-primary ">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+<?php require('./includes/footer.html'); ?>
